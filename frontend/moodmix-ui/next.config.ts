82ed3849d9f1c24143163317f32bf89c15e3ffe7
KediMengistu <kedimengistu@gmail.com>
2025-09-17 19:01:17 -0400

    Remove linting build prevention

--- frontend/moodmix-ui/next.config.ts
+++ frontend/moodmix-ui/next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ["127.0.0.1"],
+  eslint: {
+    // ✅ don’t fail `next build` on ESLint errors
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
