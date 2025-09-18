4e4f03f301f8edeb14cd519ac4f4e6058503fd8a
KediMengistu <kedimengistu@gmail.com>
2025-09-17 20:10:33 -0400

    Adjust callback component.

--- frontend/moodmix-ui/app/connect-spotify/callback/page.tsx
+++ frontend/moodmix-ui/app/connect-spotify/callback/page.tsx
@@ -1,6 +1,11 @@
 // app/connect-spotify/callback/page.tsx
+import { Suspense } from "react";
 import { ConnectSpotifyCallbackCard } from "@/app/_components/Connect-spotify-callback-card";
 
 export default function ConnectSpotifyCallbackPage() {
-  return <ConnectSpotifyCallbackCard />;
+  return (
+    <Suspense fallback={null}>
+      <ConnectSpotifyCallbackCard />
+    </Suspense>
+  );
 }
